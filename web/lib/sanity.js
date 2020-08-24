9c6b9481cb3abab0b69a7345f47432f8cdeb2b51
Nuril Baradusov <baradusovnh@gmail.com>
2020-08-24 19:29:18 +0400

    Исправил инициализацию sanity

--- web/lib/sanity.js
+++ web/lib/sanity.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: process.env.NODE_ENV === "development" ? "tgu4asx0" : process.env.SANITY_PROJECT_ID, // you can find this in sanity.json
-  dataset: process.env.NODE_ENV === "development" ? "development" : "production", // or the name you chose in step 1
+  projectId: "tgu4asx0", // you can find this in sanity.json
+  dataset: "production", // or the name you chose in step 1
   useCdn: false, // `false` if you want to ensure fresh data
 });
