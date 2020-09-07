f2652c29064908c60ca89adaffca4a6afdbdddb0
Nuril Baradusov <baradusovnh@gmail.com>
2020-09-07 20:38:17 +0400

    Описание фото теперь закрывается открывается как положено

--- web/components/Image/index.js
+++ web/components/Image/index.js
@@ -1,11 +1,12 @@
-import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { useState, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 import urlFor from '../../lib/urlFor';
 
 import styles from './index.module.css';
 
 const Image = ({ photo }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const image = useRef();
 
   const showCaption = () => {
     setIsVisible(true);
@@ -15,11 +16,31 @@ const Image = ({ photo }) => {
     setIsVisible(false);
   };
 
+  useEffect(() => {
+    console.log(isVisible);
+    if (isVisible) {
+      image.current.addEventListener('mousedown', hideCaption);
+      document.addEventListener('mousedown', hideCaption);
+      image.current.removeEventListener('mousedown', showCaption);
+    } else {
+      image.current.addEventListener('mousedown', showCaption);
+      document.removeEventListener('mousedown', hideCaption);
+
+      image.current.removeEventListener('mousedown', hideCaption);
+    }
+
+    return () => {
+      image.current.removeEventListener('mousedown', showCaption);
+      document.removeEventListener('mousedown', hideCaption);
+    };
+  }, [isVisible]);
+
   return (
     <div
       className={styles.galleryImageContainer}
       onMouseEnter={showCaption}
       onMouseLeave={hideCaption}
+      ref={image}
     >
       <motion.img
         className={styles.galleryImage}
