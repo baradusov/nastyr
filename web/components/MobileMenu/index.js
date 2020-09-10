4257fc7da4dfa16c19b15940a7e8c00d061278b3
Nuril Baradusov <baradusovnh@gmail.com>
2020-09-10 19:34:08 +0400

    Новое мобильное меню

--- web/components/MobileMenu/index.js
+++ web/components/MobileMenu/index.js
@@ -35,58 +35,75 @@ const MobileMenu = ({ pages, home }) => {
           <AnimatePresence>
             {isVisible ? null : (
               <motion.div
+                onClick={toggleMenu}
                 className={styles.secondMenu}
                 key="menu-closed"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
               >
+                <motion.svg
+                  width="21"
+                  height="5"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="2.5"
+                    cy="2.5"
+                    r="2.5"
+                    fill="#fff"
+                    fillOpacity=".4"
+                  />
+                  <motion.circle
+                    cx="10.5"
+                    cy="2.5"
+                    r="2.5"
+                    fill="#fff"
+                    fillOpacity=".4"
+                    initial={{ x: -10.5 }}
+                    animate={{ x: 0 }}
+                    exit={{ x: -10.5 }}
+                  />
+                  <motion.circle
+                    cx="18.5"
+                    cy="2.5"
+                    r="2.5"
+                    fill="#fff"
+                    fillOpacity=".4"
+                    initial={{ x: -18.5 }}
+                    animate={{ x: 0 }}
+                    exit={{ x: -18.5 }}
+                    transition={{ ease: 'easeOut' }}
+                  />
+                </motion.svg>
+
                 <p className={`${styles.menuLink} ${styles.isActive}`}>
                   {currentPage && currentPage.title}
                 </p>
-                <button className={styles.menuButton} onClick={toggleMenu}>
-                  <motion.svg
-                    width="40"
-                    height="10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <motion.circle
-                      cx="5"
-                      cy="5"
-                      r="5"
-                      fill="#fff"
-                      fillOpacity=".4"
-                      initial={{ x: 30 }}
-                      animate={{ x: 0 }}
-                      exit={{ x: 30 }}
-                      transition={{ delay: 0.05, ease: 'easeOut' }}
-                    />
-                    <motion.circle
-                      cx="20"
-                      cy="5"
-                      r="5"
-                      fill="#fff"
-                      fillOpacity=".4"
-                      initial={{ x: 15 }}
-                      animate={{ x: 0 }}
-                      exit={{ x: 15 }}
-                    />
-                    <circle cx="35" cy="5" r="5" fill="#fff" fillOpacity=".4" />
-                  </motion.svg>
-                </button>
               </motion.div>
             )}
 
             {isVisible ? (
               <motion.div
                 className={styles.secondMenuOverlay}
+                onClick={() => setIsVisible(false)}
                 key="menu-opened"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
               >
                 <div className={styles.secondMenuListContainer}>
+                  <svg
+                    style={{ display: 'block' }}
+                    width="5"
+                    height="5"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="2.5" cy="2.5" r="2.5" fill="#fff" />
+                  </svg>
+
                   <ul className={styles.menuList}>
                     {pages.map((page) => (
                       <li className={styles.menuItem} key={page._id}>
@@ -98,17 +115,6 @@ const MobileMenu = ({ pages, home }) => {
                       </li>
                     ))}
                   </ul>
-
-                  <button className={styles.menuButton} onClick={toggleMenu}>
-                    <svg
-                      width="40"
-                      height="10"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="35" cy="5" r="5" fill="#fff" />
-                    </svg>
-                  </button>
                 </div>
               </motion.div>
             ) : null}
