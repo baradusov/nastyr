8da8441a20feb3531a1c61da6743b11a04a77e19
Nuril Baradusov <baradusovnh@gmail.com>
2021-03-11 16:09:28 +0400

    Добавил тестовую метрику

--- web/components/Page/index.js
+++ web/components/Page/index.js
@@ -16,6 +16,28 @@ const Page = ({ pages, children, title, home, mainPhoto }) => {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   ym(73537873, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`,
+          }}
+        ></script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/73537873"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </Head>
 
       <Header />
--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import { Router } from 'next/router';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Router.events.on('routeChangeComplete', (url) => {
+        console.log(url);
+        ym(73537873, 'hit', url);
+      });
+    }
+
+    return () => {
+      Router.events.off('routeChangeComplete', (url) => {
+        console.log(url);
+        ym(73537873, 'hit', url);
+      });
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
