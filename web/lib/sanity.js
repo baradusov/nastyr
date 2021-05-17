503e367f20b15168a115d91686565bf081a03d9a
Nuril Baradusov <baradusovnh@gmail.com>
2021-05-17 14:13:57 +0400

    Обновил зависимости

--- web/lib/sanity.js
+++ web/lib/sanity.js
@@ -2,6 +2,7 @@ import sanityClient from "@sanity/client";
 
 export default sanityClient({
   projectId: "tgu4asx0", // you can find this in sanity.json
+  apiVersion: '2021-03-25',
   dataset: process.env.NODE_ENV, // or the name you chose in step 1
   useCdn: false, // `false` if you want to ensure fresh data
 });
