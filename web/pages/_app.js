e1c8aa5325ae0fd723931e5199a27e96db21c3df
Nuril Baradusov <baradusovnh@gmail.com>
2021-08-11 22:15:06 +0400

    Добавил фавикон

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -19,7 +19,17 @@ function MyApp({ Component, pageProps }) {
     };
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
