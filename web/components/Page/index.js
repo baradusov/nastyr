dc7502f60aa9a47c42a345e7c79292c96ab1f852
Nuril Baradusov <baradusovnh@gmail.com>
2022-03-26 22:05:28 +0300

    Вынес мета теги и метрику в _app

--- web/components/Page/index.js
+++ web/components/Page/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head';
-import Script from 'next/script';
-
 import styles from './index.module.css';
 import Header from '../Header';
 import MenuContainer from '../MenuContainer';
@@ -8,26 +5,6 @@ import MenuContainer from '../MenuContainer';
 const Page = ({ pages, children, title, home, mainPhoto }) => {
   return (
     <>
-      <Head>
-        <title>
-          {title ? `anastasia razubaeva | ${title}` : 'anastasia razubaeva'}
-        </title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/73542565"
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt=""
-            />
-          </div>
-        </noscript>
-      </Head>
-
       <Header />
 
       <main className={styles.main}>
@@ -35,15 +12,6 @@ const Page = ({ pages, children, title, home, mainPhoto }) => {
 
         {children}
       </main>
-      <Script id="metrika">{`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-   ym(73542565, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });`}</Script>
     </>
   );
 };
--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,11 +1,15 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
 import { Router } from 'next/router';
+import Script from 'next/script';
+
 import '../styles/globals.css';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
+    if (isProduction) {
       Router.events.on('routeChangeComplete', (url) => {
         ym(73542565, 'hit', url);
       });
@@ -21,12 +25,43 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <title>
+          {title ? `Anastasia Razubaeva | ${title}` : 'Anastasia Razubaeva'}
+        </title>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
+
+      {isProduction ? (
+        <>
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/73542565"
+                style={{ position: 'absolute', left: '-9999px' }}
+                alt=""
+              />
+            </div>
+          </noscript>
+          <Script id="metrika">{`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   ym(73542565, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`}</Script>
+        </>
+      ) : null}
     </>
   );
 }
